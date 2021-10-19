603fc3fcc248b60a3fa15d4065d740d84549e69d
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-19 12:58:19 +0300

    forloop

--- forloop.py
+++ forloop.py
@@ -1,7 +1,9 @@
 #We can also use a Python function named range() in our for loops. The range()function returns a list for which we can use to loop through.
 #A for loop is used when one wants to repeat something a number of times.
+for i in range(0, 7):
+    print("I would love " + str(i) + " cookies")
+    
 numbers = [1, 2, 3, 4, 5]
-
 for number in numbers:
     print(number)
 
