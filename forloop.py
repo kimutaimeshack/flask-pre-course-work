9ab98f4ccb67ff7e498cddcb7bdb011775e269ef
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-19 12:57:07 +0300

    forloop

--- forloop.py
+++ forloop.py
@@ -1,4 +1,10 @@
+#We can also use a Python function named range() in our for loops. The range()function returns a list for which we can use to loop through.
+#A for loop is used when one wants to repeat something a number of times.
 numbers = [1, 2, 3, 4, 5]
 
 for number in numbers:
     print(number)
+
+
+list_a = list(range(0, 5))
+print(list_a)
