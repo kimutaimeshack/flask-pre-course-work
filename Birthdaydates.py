17b2470906b8bb4de9fc4797611b9afe858eb7af
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-19 12:41:22 +0300

    input

---
+++ Birthdaydates.py
@@ -0,0 +1,9 @@
+#Create a program where you prompt a user for their age and return the year they were born.
+#After that, return the year they will turn 80.
+
+print("How old are you?")
+name = int(input())
+
+yearborn= 2021-name
+
+print(yearborn)
