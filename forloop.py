3e94ec15551c6c21756213de43d82a9f036aeef2
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-19 13:01:24 +0300

    forloop

--- forloop.py
+++ forloop.py
@@ -2,11 +2,10 @@
 #A for loop is used when one wants to repeat something a number of times.
 for i in range(0, 7):
     print("I would love " + str(i) + " cookies")
-    
-numbers = [1, 2, 3, 4, 5]
-for number in numbers:
-    print(number)
 
 
-list_a = list(range(0, 5))
-print(list_a)
+
+list_a = list(range(0, 20))
+for i in list_a:
+    if i % 2 == 0:
+        print(i)
